3771a8f515368844dbe4ac4a11801c4f12db0d3b
sanjar228 <sanjarforgit@gmail.com>
2024-11-23 00:09:39 +0600

    Added app in settings, created model

--- todoist/todoapp/models.py
+++ todoist/todoapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+status_choices = [('new', 'Новая'),('in_progress','В процессе'), ('done','Сделано')]
+
+class Todo(models.Model):
+    desc = models.CharField(max_length=256, verbose_name='Описание')
+    status = models.CharField(max_length=64, choices=status_choices, verbose_name='Статус', default='new')
+    date = models.DateField(null=True, blank=True)
