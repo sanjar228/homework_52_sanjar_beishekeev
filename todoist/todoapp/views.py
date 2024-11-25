6a17e3bd44a7f9e7f7faf0ef81fa0321bb6e9c9f
sanjar228 <sanjarforgit@gmail.com>
2024-11-25 20:25:28 +0600

    made ToDo app

--- todoist/todoapp/views.py
+++ todoist/todoapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo, status_choices
 
-# Create your views here.
+
+
+def main(request):
+    if request.method == 'GET':
+        t = Todo.objects.all()
+        return render(request, 'main.html', {'todos':t, 'statuses': status_choices})
+    elif request.method == 'POST':
+        desc = request.POST.get('desc')
+        status = request.POST.get('status')
+        date = request.POST.get('date')
+        temp  = Todo(
+            desc = desc,
+            status = status,
+            date = date
+        )
+        temp.save()
+        return redirect('/')
